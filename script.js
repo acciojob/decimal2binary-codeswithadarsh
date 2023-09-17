00c0a293aade4517a5f99c22ea04c3816fd2a3b8
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-17 21:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function decimalToBinary(num) {
+	
   //Write you code here
+	let sum=""
+	if(num==0)
+	{
+		return 0;
+	}
+	while(num>0)
+		{
+			sum=(num%2)+sum;
+			num=Math.floor(num/2);
+		}
+	return sum;
   
 }
 
